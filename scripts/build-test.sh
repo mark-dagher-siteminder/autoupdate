ea8319f35ad2b005ed1fa769cd0c6f56b72188f9
Mark Dagher <mark.dagher@siteminder.com>
2024-08-22 17:03:18 +1000

    update repo

--- scripts/build-test.sh
+++ scripts/build-test.sh
@@ -1,20 +1,20 @@
 #!/usr/bin/env bash
 
-set -e
-set -o nounset
+# set -e
+# set -o nounset
 
-SCRIPT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
-REPO="chinthakagodawita/autoupdate-action-unstable"
+# SCRIPT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
+# REPO="chinthakagodawita/autoupdate-action-unstable"
 
-pushd "${SCRIPT_DIR}/.." > /dev/null
+# pushd "${SCRIPT_DIR}/.." > /dev/null
 
-set -x
+# set -x
 
-docker build -t "${REPO}:v1" .
-docker tag "${REPO}:v1" "${REPO}:latest"
-docker push "${REPO}:v1"
-docker push "${REPO}:latest"
+# docker build -t "${REPO}:v1" .
+# docker tag "${REPO}:v1" "${REPO}:latest"
+# docker push "${REPO}:v1"
+# docker push "${REPO}:latest"
 
-set +x
+# set +x
 
-popd > /dev/null
+# popd > /dev/null
